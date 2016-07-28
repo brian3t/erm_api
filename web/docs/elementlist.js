4eb49ad682084359df536d1f37f030ac42fdb17c
brian3t <ngxtri@gmail.com>
2016-07-28 11:15:45 -0700

    Questionable vendor/mootensai/yii2-enhanced-gii

--- web/docs/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","app\\controllers\\EventController"],["c","app\\controllers\\InventoryController"],["c","app\\controllers\\MpController"],["c","app\\controllers\\OrderController"],["c","app\\controllers\\OrderItemController"],["c","app\\controllers\\SiteController"],["c","app\\controllers\\TrackingController"],["c","app\\mp\\Agent"],["c","app\\rop\\Agent"]];
