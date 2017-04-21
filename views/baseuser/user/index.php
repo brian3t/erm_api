defb9a10426b41ed89b8dcac736f61a38f9e0d5e
brian3t <ngxtri@gmail.com>
2017-04-20 22:43:21 -0700

    Big update: company and user.

--- views/baseuser/user/index.php
+++ views/baseuser/user/index.php
@@ -53,9 +53,21 @@ $this->registerJs($search);
         'birthdate:date',
         'website_url:url',
         [
+            'attribute' => 'belong_company_id',
+            'label' => 'Belongs to',
+            'value' => function($model){
+                return $model->belongCompany->name??'';
+            },
+            'filterType' => GridView::FILTER_SELECT2,
+            'filter' => \yii\helpers\ArrayHelper::map(\app\models\Company::find()->asArray()->all(), 'id', 'name'),
+            'filterWidgetOptions' => [
+                'pluginOptions' => ['allowClear' => true],
+            ],
+            'filterInputOptions' => ['placeholder' => 'Company', 'id' => 'grid--belong_company_id']
+        ],[
             'class' => 'yii\grid\ActionColumn',
         ],
-    ]; 
+    ];
     ?>
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
