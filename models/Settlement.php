d77b87c3c645ffb577e63ea0657aa302335d39d0
brian3t <ngxtri@gmail.com>
2017-04-28 12:38:26 -0700

    Fix settlement validate.

--- models/Settlement.php
+++ models/Settlement.php
@@ -10,4 +10,9 @@ use \app\models\base\Settlement as BaseSettlement;
 class Settlement extends BaseSettlement
 {
     public static $order_by_col='settlement_id';
+    public function beforeValidate()
+    {
+        $this->settlement_id = strval($this->settlement_id);
+        return parent::beforeValidate();
+    }
 }
