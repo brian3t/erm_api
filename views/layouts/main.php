47d5b15fa05150dd8cb5df5f0ec872abb8424e94
brian3t <ngxtri@gmail.com>
2017-05-23 23:05:52 -0700

    v1.4.7

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -36,7 +36,7 @@ AppAsset::register($this);
             'class' => 'navbar navbar-fixed-top',
         ],
     ]);
-    echo '<span class="navbar-brand">v1.4.5</span>';
+    echo '<span class="navbar-brand">v1.4.7</span>';
     $items = [];
     $admin_items = [
     
